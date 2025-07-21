a54e336a12c17b2e60fbac7d3add43a3ff5007ce
Gian <giansantos.dev@gmail.com>
2025-07-21 15:14:33 -0300

    add item on home component

---
+++ frontend/src/components/ItemOnHome.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import goldImage from '@/assets/gold.png'
+import silverImage from '@/assets/silver.png'
+
+defineProps<{
+  id: number
+  name: string
+  price: {
+    gold: number
+    silver: number
+  }
+  quality: number
+  rarity: 'COMMON' | 'UNCOMMON' | 'RARE' | 'EPIC' | 'LEGENDARY' | 'ARTIFACT' | 'TOKEN'
+  image: string
+}>()
+
+const rarityColors = {
+  COMMON: 'border-common',
+  UNCOMMON: 'border-uncommon',
+  RARE: 'border-rare',
+  EPIC: 'border-epic',
+  LEGENDARY: 'border-legendary',
+  ARTIFACT: 'border-artifact',
+  TOKEN: 'border-token',
+}
+</script>
+<template>
+  <router-link
+    :to="`/item/${id}`"
+    class="flex max-w-[264px] gap-3 rounded p-2 transition-colors hover:bg-white/5"
+  >
+    <img :src="image" :alt="name" :class="`${rarityColors[rarity]} h-16 w-16 rounded border-2`" />
+    <div class="flex flex-col items-start justify-between">
+      <p class="overflow-hidden text-2xl font-semibold text-ellipsis whitespace-nowrap">
+        {{ name }}
+      </p>
+      <div class="text-light-yellow flex items-center gap-1.5">
+        <div class="flex items-center gap-0.5">
+          <p>{{ price.gold }}</p>
+          <img :src="goldImage" alt="" class="h-4 w-4" />
+        </div>
+        <div class="flex items-center gap-0.5">
+          <p>{{ price.silver }}</p>
+          <img :src="silverImage" alt="" class="h-4 w-4" />
+        </div>
+      </div>
+    </div>
+  </router-link>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,67 @@
+<script setup lang="ts">
+import ItemOnHome from '@/components/ItemOnHome.vue'
+import { ref } from 'vue'
+
+const hoursAndItems = ref<
+  {
+    hour: string
+    items: {
+      id: number
+      name: string
+      price: {
+        gold: number
+        silver: number
+      }
+      quality: number
+      rarity: 'COMMON' | 'UNCOMMON' | 'RARE' | 'EPIC' | 'LEGENDARY' | 'ARTIFACT' | 'TOKEN'
+      image: string
+    }[]
+  }[]
+>([
+  {
+    hour: '00:00',
+    items: [
+      {
+        id: 2,
+        name: 'Item 1',
+        price: {
+          gold: 16,
+          silver: 67,
+        },
+        quality: 0,
+        rarity: 'LEGENDARY',
+        image: 'https://picsum.photos/200',
+      },
+    ],
+  },
+])
+</script>
+
 <template>
-  <main>
-    <h1 class="text-3xl font-bold">Oh, hi!</h1>
+  <main class="mt-6">
+    <h2 class="font-title text-3xl font-bold">Itens para vender hoje</h2>
+
+    <div class="mt-6 flex flex-col gap-8">
+      <div
+        v-for="hour in hoursAndItems"
+        :key="hour.hour"
+        class="bg-midnight-light-200 rounded-lg p-4 pb-6"
+      >
+        <h3 class="font-title text-2xl font-bold">{{ hour.hour }}</h3>
+        <div>
+          <item-on-home
+            v-for="item in hour.items"
+            :key="item.name"
+            :id="item.id"
+            :name="item.name"
+            :price="item.price"
+            :quality="item.quality"
+            :image="item.image"
+            :rarity="item.rarity"
+          >
+          </item-on-home>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
