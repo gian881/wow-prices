a6bdbe361c9ac21e0f65bc1548bc114a75ca9550
Gian <giansantos.dev@gmail.com>
2025-09-22 09:57:30 -0300

    fix: removed prints from prod

--- frontend/src/services/websocketService.ts
+++ frontend/src/services/websocketService.ts
@@ -14,20 +14,13 @@ export const websocketService = {
       return
     }
 
-    console.log('Iniciando conexão WebSocket... com URL:', URL)
-
     socket = new WebSocket(URL)
-
-    console.log('Tentando conectar ao WebSocket...')
 
     socket.onopen = () => {
       state.isConnected = true
-      console.log('WebSocket conectado com sucesso.')
     }
 
     socket.onmessage = (event) => {
-      console.log('Mensagem recebida do WebSocket:', event.data)
-
       try {
         state.lastMessage = JSON.parse(event.data)
       } catch (error) {
@@ -41,7 +34,6 @@ export const websocketService = {
 
     socket.onclose = () => {
       state.isConnected = false
-      console.log('WebSocket desconectado. Tentando reconectar...')
       setTimeout(() => {
         this.connect()
       }, 5000)
@@ -60,7 +52,6 @@ export const websocketService = {
   },
 
   sendJSONMessage(message: object) {
-    console.log('Enviando mensagem pelo WebSocket:', message)
     if (socket && socket.readyState === WebSocket.OPEN) {
       socket.send(JSON.stringify(message))
     } else {
